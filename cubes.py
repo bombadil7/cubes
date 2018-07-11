da1e0becb76bdd9d151d874fec30165defc08326
Andrei Kniazev <akniazev@gmail.com>
2018-07-11 02:21:52 +0000

    Add key generator to encoder class.

--- cubes.py
+++ cubes.py
@@ -1,5 +1,6 @@
 import math
 import sys
+import random
 
 """
          b2_____b3
@@ -24,7 +25,7 @@ class Cube:
 # Create an empty dictionary.  This way the cube has all the corners.
         self.ver = {}
         for k in self.corners:
-            self.ver[k] = ''
+            self.ver[k] = '?'
 
 # Fill in the corners with available letters
         mes = [message[i] for i in range(len(message))]
@@ -61,43 +62,58 @@ class Cube:
 
         
 
-#class Encoder():
-#    def encode(self, phrase, key):
-#"""
-#        encode receives a phrase to encrypt and a key, and 
-#        it returns the encrypted version of that phrase.
-#
-#        The number of cubes required and rotation sequence comes
-#        as a key.  
-#"""
-#
-#        return encoded_phrase 
-#
-#
-#    def decode(self, phrase ,key):
-#"""
-#        decode receives an encrypted phrase and a key, and returns
-#        that phrase decrypted per the key's instructions.
-#"""
-#
-#        return word
-#
-#    def keyGenerator(self, phrase):
-#"""
-#        keyGenerator is just a utility function, which makes generating
-#        random encryption keys easier.
-#        All it does is return a new random key back to the caller.
-#
-#        In order to know how many cubes to use for the encryption we 
-#        need the phrase as an input.
-#"""
-#        num_cubes = math.ceil(len(phrase) / 8)  # there are 8 corners for 8 letters
-#
-#        for c in range(num_cubes):
-#
-#
-#
-#        return key
+class Encoder():
+    def encode(self, phrase, key):
+        """
+        encode receives a phrase to encrypt and a key, and 
+        it returns the encrypted version of that phrase.
+
+        The number of cubes required and rotation sequence comes
+        as a key.  
+        """
+
+        return encoded_phrase 
+
+
+    def decode(self, phrase ,key):
+        """
+        decode receives an encrypted phrase and a key, and returns
+        that phrase decrypted per the key's instructions.
+        """
+
+        return word
+
+    def keyGenerator(self, phrase):
+        """
+        keyGenerator is just a utility function, which makes generating
+        random encryption keys easier.
+        All it does is return a new random key back to the caller.
+
+        In order to know how many cubes to use for the encryption we 
+        need the phrase as an input.
+
+        An example of an encryption key is:
+        0:U:U:L:D,1:D,2:L:U:L
+        Here cube 0 is rotated up, up, left, down,
+        cube 1 is rotated down, and cube 2 is rotated left, up, left
+        """
+        # First determine how many cubes are required to hold the message
+        # Last cube may not be filled completely
+        num_cubes = math.ceil(len(phrase) / 8)  # there are 8 corners for 8 letters
+
+        # Now for each cube we assign a random number of rotations in random directions
+        dirs = ['U', 'D', 'L', 'R']
+        key = ''
+        for c in range(num_cubes):
+            if c > 0:
+                key.join(',')
+            key.join(str(c))
+            print(key)
+            # Select random number of rotations
+            for rot in range(1, random.randint(0, 5)):
+                # Each time we pick a random direction
+                key.join(':' + dirs[random.randint(0, len(dirs) - 1)])
+        return key
 
 if __name__=='__main__':
     if  len(sys.argv) > 1:
@@ -106,11 +122,13 @@ if __name__=='__main__':
     else:
         phrase = 'Secure'
 
-    cube = Cube(phrase)
-    print(cube.print_message())
-    cube.rotate_up()
-    cube.rotate_down()
-    cube.rotate_left()
-    cube.rotate_right()
-    print(cube.print_message())
+#    cube = Cube(phrase)
+#    print(cube.print_message())
+#    cube.rotate_up()
+#    cube.rotate_down()
+#    cube.rotate_left()
+#    cube.rotate_right()
+#    print(cube.print_message())
 
+    encoder = Encoder()
+    print(encoder.keyGenerator(phrase))
